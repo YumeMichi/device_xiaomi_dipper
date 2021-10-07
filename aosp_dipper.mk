5759687fd65d5c03fe7e28155cf893e3fe661921
YumeMichi <do4suki@gmail.com>
2021-10-07 23:55:36 +0800

    dipper: AOSP bringup
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- aosp_dipper.mk
+++ aosp_dipper.mk
@@ -7,10 +7,13 @@
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := aosp_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
