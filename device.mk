27247ebbd8f8b2ee596e627c3b5ba8e79e165bd9
YumeMichi <do4suki@gmail.com>
2021-10-10 23:31:26 +0800

    ooo
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -107,6 +107,12 @@ PRODUCT_PACKAGES += \
 
 # Secure element
 PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.1 \
+    android.hardware.secure_element@1.1.vendor \
+    android.hardware.secure_element@1.2 \
+    android.hardware.secure_element@1.2.vendor \
     SecureElement
 
 # Soong namespaces
--- lemon_dipper.mk
+++ lemon_dipper.mk
@@ -7,13 +7,10 @@
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
-
-# Bootanimation
-TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/lemon/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_dipper
+PRODUCT_NAME := lemon_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
